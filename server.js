e02bc844eaac135904e9d6c3ab44c91651465728
johnlevengood <50719877+johnlevengood@users.noreply.github.com>
2020-12-20 15:01:09 -0500

    first try

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 
 // init project
 var express = require('express');
+const PORT = process.env.PORT || 3000
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
+app.use(express.json())
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -24,9 +25,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/whoami', (req,res) => {
+    const resp = {
+        ipaddress: req.ip.toString(),
+        language:req.headers['accept-language'],
+        software:req.headers['user-agent']
+    }
+    res.status(200).send(resp)
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(PORT, function () {
+  console.log('Your app is listening on port ' + PORT);
 });
